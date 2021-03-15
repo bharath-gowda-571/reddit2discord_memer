7035b1f5da862a1b8d5c6b1c5a0c0fc5c6fcefe3
Bharath Gowda <bharath1234gowda@gmail.com>
2021-03-15 22:52:42 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import tweepy
 
-auth=tweepy.OAuthHandler("tFeGrVNDfR4CAPLEEx2Tt52l2","AiCxk3I2NoGK4SgAzqK57VSQIQpwQXSUBiWToSDQacvKJRVxFQ")
-auth.set_access_token("816185268390793216-1jqnlBk4kam4rtHBQKYMdqzdbEdJg50","b7M7rpNCzoVtapkAZQz8jwzzMcvYyULQOSWSsuFwzapne")
+auth=tweepy.OAuthHandler("********","***********")
+auth.set_access_token("************","**************")
 api=tweepy.API(auth)
 
 def tweet_image(filename,message):
